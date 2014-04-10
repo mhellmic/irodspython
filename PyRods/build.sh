6a4da633374a3f43904d95758d06c86d5240eb40
Martin Hellmich <mhellmic@gmail.com>
2014-04-10 12:05:44 +0200

    add a pre-generated irods_wrap.c and precompiled irods.py
    Running swig on this codebase is error-prone.
    It seems you have to use the right swig version (2.0.10) and
    then overwrite one function that swig generates wrongly for you.
    
    I don't know where this functions comes from as the original
    source also delivers a pre-generated irods_wrap.c
    
    add a patch application in build.sh that replaces the function
    with what it's suppposed to be. Now you should be able to run
    ./build.sh with a correct outcome at least if you have
    swig 2.0.10.

--- PyRods/build.sh
+++ PyRods/build.sh
@@ -3,6 +3,13 @@
 # Create irodsswig_wrap.c
 swig -python -Iswig irods.i
 
+# apply patch to fix a function
+# my swig version always creates the function wrongly
+# this happens:
+#    lastStatus = _irods.splitPathByKey(srcPath, coll, data, key)
+#       TypeError: in method 'splitPathByKey', argument 1 of type 'char *'
+patch -R -p0 irods_wrap.c < SWIG_AsCharPtrAndSize_function.patch
+
 # create _irods.so
 python setup.py build_ext
 
